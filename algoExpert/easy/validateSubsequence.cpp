389fa5bd51255205c8997de906a7ed30c2ff0ab8
koadar <timewaaste@gmail.com>
2022-02-26 14:38:16 +0530

    Solution (validateSubsequence) : Written the code in c++.

---
+++ algoExpert/easy/validateSubsequence.cpp
@@ -0,0 +1,26 @@
+using namespace std;
+
+bool isValidSubsequence(vector<int> array, vector<int> sequence) {
+	
+	auto it   = sequence.begin();
+	auto it2  = array.begin();
+	
+	while (it != sequence.end() )
+	{
+		if (*it == *it2)
+			{
+				it++;
+				it2++;
+			}
+		else
+			{
+				it2++;
+				if (it2 == array.end() || it2-1 ==array.end())
+				{
+					return false;
+				}
+			}
+	}
+  return true;
+}
+
